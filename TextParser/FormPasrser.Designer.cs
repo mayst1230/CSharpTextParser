f1e099ac1d65a227b87e6f1387705de951d8a4d8
ya.maiorov <ya.maiorov@aisgorod.ru>
2021-12-21 12:49:43 +0400

    Create text parser

---
+++ TextParser/FormPasrser.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace TextParser
+{
+    partial class FormPasrser
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.folderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkedListFiles = new System.Windows.Forms.CheckedListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.folderToolStripMenuItem,
+            this.fileToolStripMenuItem,
+            this.clearListToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1134, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // folderToolStripMenuItem
+            // 
+            this.folderToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.folderToolStripMenuItem.Name = "folderToolStripMenuItem";
+            this.folderToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
+            this.folderToolStripMenuItem.Text = "Folder";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(123, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // clearListToolStripMenuItem
+            // 
+            this.clearListToolStripMenuItem.Name = "clearListToolStripMenuItem";
+            this.clearListToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+            this.clearListToolStripMenuItem.Text = "Clear";
+            this.clearListToolStripMenuItem.Click += new System.EventHandler(this.clearListToolStripMenuItem_Click);
+            // 
+            // checkedListFiles
+            // 
+            this.checkedListFiles.CheckOnClick = true;
+            this.checkedListFiles.FormattingEnabled = true;
+            this.checkedListFiles.Location = new System.Drawing.Point(0, 33);
+            this.checkedListFiles.Name = "checkedListFiles";
+            this.checkedListFiles.Size = new System.Drawing.Size(1134, 510);
+            this.checkedListFiles.TabIndex = 1;
+            // 
+            // FormPasrser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 539);
+            this.Controls.Add(this.checkedListFiles);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormPasrser";
+            this.Text = "CSharpPasrser";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem folderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.CheckedListBox checkedListFiles;
+        private System.Windows.Forms.ToolStripMenuItem clearListToolStripMenuItem;
+    }
+}
---
+++ TextParser/FormPasrser.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TextParser
+{
+    public partial class FormPasrser : Form
+    {
+        private List<string> Files = new List<string>();
+        private readonly string[] Extensions = new string[2] { "*.cs", "*.xaml" };
+
+
+        public FormPasrser()
+        {
+            InitializeComponent();
+        }
+
+        private bool OpenFolder(string path)
+        {
+            foreach (string extension in Extensions)
+            {
+                var directoryWithAllFiles = Directory.GetFiles(path, extension, SearchOption.AllDirectories).ToList();
+
+                foreach (var file in directoryWithAllFiles)
+                {
+                    Files.Add(file);
+                }
+            }
+
+            if (Files.Count > 0)
+                return true;
+
+            return false;
+        }
+
+        private bool SaveFile(string path)
+        {
+            var streamWriter = new StreamWriter(path, true);
+
+            for (int file = 0; file < checkedListFiles.Items.Count; file++)
+            {
+                if (checkedListFiles.GetItemChecked(file))
+                {
+                    var lines = File.ReadAllLines(checkedListFiles.GetItemText(checkedListFiles.Items[file]));
+                    foreach (string line in lines)
+                    {
+                        streamWriter.WriteLine(line);
+                    };
+                }
+            }
+            streamWriter.Close();
+
+            return true;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(fbd.SelectedPath))
+                    MessageBox.Show("Папка не выбрана", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                if (OpenFolder(fbd.SelectedPath))
+                {
+                    foreach (var file in Files)
+                    {
+                        checkedListFiles.Items.Add(file);
+                    }
+
+                    for (int @checked = 0; @checked < Files.Count; @checked++)
+                    {
+                        checkedListFiles.SetItemChecked(@checked, true);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка при выгрузке всех файлов", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var svf = new SaveFileDialog();
+            svf.Filter = "All texts documents(*.txt)|*.txt";
+
+            if (svf.ShowDialog() == DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(svf.FileName))
+                    MessageBox.Show("Путь не указан", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                if (SaveFile(svf.FileName))
+                    MessageBox.Show("Файл сохранен по пути: " + svf.FileName);
+                else
+                    MessageBox.Show("Ошибка при сохранении файла", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void clearListToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (var element in Files)
+            {
+                checkedListFiles.Items.Remove(element);
+            }
+        }
+    }
+}
